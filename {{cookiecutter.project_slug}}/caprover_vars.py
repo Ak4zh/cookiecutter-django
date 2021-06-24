6ed69dbd63d5ae04a897ac1dcccd146398fc496d
Akash Agarwal <agwl.akash@gmail.com>
2021-06-25 03:15:23 +0530

    fixed missing database_url env

--- {{cookiecutter.project_slug}}/caprover_vars.py
+++ {{cookiecutter.project_slug}}/caprover_vars.py
@@ -12,13 +12,13 @@ def setup():
         cap_namespace = env("CAPROVER_NAMESPACE")
         redis_url = f"redis://:{env('REDIS_PASSWORD')}@srv-captain--{cap_namespace}-redis:6379/0"
         postgres_host = f"srv-captain--{cap_namespace}-postgres-db"
-        # database_url = f"postgres://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}" \
-        #                f"@{postgres_host}:{env('POSTGRES_PORT')}/{env('POSTGRES_DB')}"
+        database_url = f"postgres://{env('POSTGRES_USER')}:{env('POSTGRES_PASSWORD')}" \
+                       f"@{postgres_host}:{env('POSTGRES_PORT')}/{env('POSTGRES_DB')}"
 
+        caprover_vars["CELERY_BROKER_URL"] = redis_url
         caprover_vars["REDIS_URL"] = redis_url
         caprover_vars["POSTGRES_HOST"] = postgres_host
-        if caprover_vars.get("DATABASE_URL"):
-            del caprover_vars["DATABASE_URL"]
+        caprover_vars["DATABASE_URL"] = database_url
 
         for k, v in caprover_vars.items():
             if v and isinstance(v, str) and k not in ["DJANGO_ALLOWED_HOSTS"]:
